c87a50cdd44f806d2cc2ed231c45ca1135ed8269
Jggranito <jggranito@gmail.com>
2023-01-09 18:59:13 -0300

    Melhorando a validação para não considerar strings compostas apenas por espaços em branco.

--- src/js/validaFormulario.js
+++ src/js/validaFormulario.js
@@ -7,7 +7,7 @@ const inputCampo = document.querySelectorAll(".campo-formulario")
 function validaFormulario() {
     inputCampo.forEach(function (input) {
         if (input.childNodes[1].nodeName === "INPUT") {
-            if (input.childNodes[1].value === "") {
+            if (input.childNodes[1].value === "" || !input.childNodes[1].value.trim()) {
                 input.classList.add('campo-invalido')
                 input.classList.remove('campo-valido')
             } else {
@@ -15,7 +15,7 @@ function validaFormulario() {
                 input.classList.add('campo-valido')
             }
         } else if (input.childNodes[3].nodeName === "TEXTAREA") {
-            if (input.childNodes[3].value === "") {
+            if (input.childNodes[3].value === "" || !input.childNodes[3].value.trim()) {
                 input.classList.add('campo-invalido')
                 input.classList.remove('campo-valido')
             } else {
